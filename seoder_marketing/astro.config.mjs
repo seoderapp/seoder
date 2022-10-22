67665b9014daa74e92f8d77b477813a3801474db
j-mendez <jeff@a11ywatch.com>
2022-10-22 14:15:03 -0400

    chore(stores): add nano store setup

--- seoder_marketing/astro.config.mjs
+++ seoder_marketing/astro.config.mjs
@@ -1,10 +1,13 @@
 import { defineConfig } from "astro/config";
 import react from "@astrojs/react";
 import vercel from "@astrojs/vercel/serverless";
+import robotsTxt from "astro-robots-txt";
+import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
+  site: "https://seoder.io",
   output: "server",
   adapter: vercel(),
-  integrations: [react()],
+  integrations: [react(), robotsTxt(), sitemap()],
 });
---
+++ src-tauri/www/stores/profile.ts
@@ -0,0 +1,5 @@
+import { map } from "nanostores";
+
+export const profile = map({
+  name: "anonymous",
+});
