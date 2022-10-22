c0049bbeac212f7c80e132c8de091ce56899bbd1
j-mendez <jeff@a11ywatch.com>
2022-10-21 23:19:48 -0400

    chore(ui): add stateful license form

---
+++ src-tauri/www/components/LicenseInput.tsx
@@ -0,0 +1,46 @@
+import { useState, SyntheticEvent } from "react";
+import "../css/license.css";
+
+export default function LicenseInput() {
+  const [license, setLicense] = useState<string>("");
+
+  const onChangeTextEvent = (e: SyntheticEvent<HTMLFormElement>) => {
+    setLicense(e.target.value);
+  };
+
+  const disabled = license.length < 30;
+
+  const clear = (e) => {
+    e.preventDefault();
+    const ele = document.getElementById("settings-container");
+    if (ele && ele.className !== "hidden") {
+      ele.className = "hidden";
+    }
+  };
+
+  return (
+    <>
+      <div className="license-form">
+        <label htmlFor="license">License Key</label>
+        <input
+          name="license"
+          placeholder="XXXX-XXXX-XXXXX-XXXXX-XXXXXX"
+          type="text"
+          className="license-input"
+          onChange={onChangeTextEvent}
+        />
+        <div className="gutter-t">
+          <button
+            type="submit"
+            style={{ opacity: disabled ? 0.6 : 1 }}
+            className={"submit"}
+            disabled={disabled}
+            onClick={clear}
+          >
+            Verify License Key
+          </button>
+        </div>
+      </div>
+    </>
+  );
+}
