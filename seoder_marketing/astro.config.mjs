611fa386bba4c8e1bd3232159b7cca368766d9c8
j-mendez <jeff@a11ywatch.com>
2022-11-10 12:18:19 -0500

    chore(marketing): update sitemap pages

--- seoder_marketing/astro.config.mjs
+++ seoder_marketing/astro.config.mjs
@@ -4,7 +4,6 @@ import vercel from "@astrojs/vercel/serverless";
 import robotsTxt from "astro-robots-txt";
 import sitemap from "@astrojs/sitemap";
 import image from "@astrojs/image";
-
 const site = process.env.DOMAIN || "https://seoder.com";
 
 // https://astro.build/config
@@ -14,15 +13,20 @@ export default defineConfig({
   adapter: vercel(),
   integrations: [
     react(),
-    robotsTxt({ sitemap: true }),
     sitemap({
       customPages: [
         "/",
+        "/about",
         "/payments",
         "/faq",
         "/privacy-policy",
         "/terms-of-service",
+        "/seos-can-be-dangerous",
+        "/compare/seoder-vs-zendesk-sell",
       ],
+    }),
+    robotsTxt({
+      sitemap: true,
     }),
     image({
       serviceEntryPoint: "@astrojs/image/sharp",
